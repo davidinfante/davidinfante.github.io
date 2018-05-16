b8444b02b5eef9f5b3b7c8fe1170b813febca850
TheikACR <davidinfante1997@gmail.com>
2018-05-16 15:43:16 +0200

    new version

--- fps/avatar.js
+++ fps/avatar.js
@@ -20,6 +20,8 @@ class Avatar {
         this.goingUp = true;
         this.recoil = true;
         this.posLimite = 82;
+        this.shotgun = null;
+        this.rifle = null;
 
         this.avatar.add(this.camera);
     }
@@ -94,16 +96,16 @@ class Avatar {
 
     changeWeapon() {
         if (this.activeWeapon == 0) {
-            this.camera.children[2].material.transparent = true;
-            this.camera.children[2].material.opacity = 0.0;
-            this.camera.children[1].material.transparent = false;
-            this.camera.children[1].material.opacity = 1.0;
+            this.rifle.material.transparent = true;
+            this.rifle.material.opacity = 0.0;
+            this.shotgun.material.transparent = false;
+            this.shotgun.material.opacity = 1.0;
             this.activeWeapon = 1;
         } else if (this.activeWeapon == 1) {
-            this.camera.children[1].material.transparent = true;
-            this.camera.children[1].material.opacity = 0.0;
-            this.camera.children[2].material.transparent = false;
-            this.camera.children[2].material.opacity = 1.0;
+            this.shotgun.material.transparent = true;
+            this.shotgun.material.opacity = 0.0;
+            this.rifle.material.transparent = false;
+            this.rifle.material.opacity = 1.0;
             this.activeWeapon = 0;
         }
     }
@@ -111,23 +113,23 @@ class Avatar {
     animateWeapon() {
         if (this.activeWeapon == 0) {
             if (this.recoil) {
-                if (this.camera.children[2].rotation.x >= 0.2) this.recoil = false;
-                else this.camera.children[2].rotation.x += 0.1;
+                if (this.rifle.rotation.x >= 0.2) this.recoil = false;
+                else this.rifle.rotation.x += 0.1;
             } else {
-                if (this.camera.children[2].rotation.x >= 0 && this.camera.children[2].rotation.x <= 0.1) {
+                if (this.rifle.rotation.x >= 0 && this.rifle.rotation.x <= 0.1) {
                     disparando = false;
                     this.recoil = true;
-                } else this.camera.children[2].rotation.x -= 0.1;
+                } else this.rifle.rotation.x -= 0.1;
             }
         } else if (this.activeWeapon == 1) {
             if (this.recoil) {
-                if (this.camera.children[1].rotation.x >= 1.8) this.recoil = false;
-                else this.camera.children[1].rotation.x += 0.1;
+                if (this.shotgun.rotation.x >= 1.8) this.recoil = false;
+                else this.shotgun.rotation.x += 0.1;
             } else {
-                if (this.camera.children[1].rotation.x >= 0 && this.camera.children[1].rotation.x <= 0.1) {
+                if (this.shotgun.rotation.x >= 0 && this.shotgun.rotation.x <= 0.1) {
                     disparando = false;
                     this.recoil = true;
-                } else this.camera.children[1].rotation.x -= 0.1;
+                } else this.shotgun.rotation.x -= 0.1;
             }
         }
     }
@@ -140,35 +142,35 @@ class Avatar {
         var objLoader = new THREE.OBJLoader();
         var texture = null;
 
-        mtlLoader.setPath( "fps/models/" );
+        mtlLoader.setPath( "models/" );
         mtlLoader.load( "material.mtl" , function ( materials ) {
             materials.preload();
             
             objLoader.setMaterials( materials );
-            objLoader.setPath( "fps/models/" );
+            objLoader.setPath( "models/" );
             objLoader.load( "m4a1_s.obj", function ( object ) {
-                texture = THREE.ImageUtils.loadTexture('fps/models/m4a1_stext.png');
+                texture = THREE.ImageUtils.loadTexture('models/m4a1_stext.png');
                 object.children[1].material = new THREE.MeshLambertMaterial({map: texture});
-
                 //m4a1_s
                 object.children[1].position.set(0, 0, 0);
                 object.children[1].scale.set(0.2, 0.2, 0.2);
                 object.children[1].rotation.set(0.1, 3.4, 0);
                 object.children[1].position.set(2, -0.8, -2);
-                thatCamera.add(object.children[1]);
+                that.rifle = object.children[1];
+                thatCamera.add(that.rifle);
                 that.activeWeapon = 0;
 
             });
         });
 
-        mtlLoader.setPath( "fps/models/" );
+        mtlLoader.setPath( "models/" );
         mtlLoader.load( "material.mtl" , function ( materials ) {
             materials.preload();
 
             objLoader.setMaterials( materials );
-            objLoader.setPath( "fps/models/" );
+            objLoader.setPath( "models/" );
             objLoader.load( "escopeta.obj", function ( object ) {
-                texture = THREE.ImageUtils.loadTexture('fps/models/escopetatext.png');
+                texture = THREE.ImageUtils.loadTexture('models/escopetatext.png');
                 object.children[0].material = new THREE.MeshLambertMaterial({map: texture});
                 
                 //Escopeta
@@ -178,7 +180,8 @@ class Avatar {
                 object.children[0].position.set(2, -1.4 , -6);
                 object.children[0].material.transparent = true;
                 object.children[0].material.opacity = 0.0;
-                thatCamera.add(object.children[0]);
+                that.shotgun = object.children[0];
+                thatCamera.add(that.shotgun);
 
             });
         });
